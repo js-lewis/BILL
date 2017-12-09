a749b977be77de3f90271fc5de9b30644bc8cd84
pmourfield <peter.mourfield@gmail.com>
2017-12-09 11:09:03 -0500

    Expanded test coverage

--- src/test/java/edu/sc/csce740/model/UserModelTest.java
+++ src/test/java/edu/sc/csce740/model/UserModelTest.java
@@ -1,4 +1,29 @@
 package edu.sc.csce740.model;
 
+import edu.sc.csce740.defines.College;
+import edu.sc.csce740.defines.Role;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserModelTest {
+
+    @Test
+    public void testUserToStringGTZero(){
+        User testUser = new User();
+        Assert.assertTrue(testUser.toString().length() > 0);
+    }
+
+    @Test
+    public void testUserSetGetRole(){
+        User testUser = new User();
+        testUser.setRole(Role.STUDENT);
+        Assert.assertEquals(Role.STUDENT, testUser.getRole());
+    }
+
+    @Test
+    public void testUserSetGetCollege(){
+        User testUser = new User();
+        testUser.setCollege(College.GRADUATE_SCHOOL);
+        Assert.assertEquals(College.GRADUATE_SCHOOL, testUser.getCollege());
+    }
 }
